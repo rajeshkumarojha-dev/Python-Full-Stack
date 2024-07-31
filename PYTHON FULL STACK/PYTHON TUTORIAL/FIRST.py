a77383644dc596c89f8fce108612e977cdfd72f4
Rajesh kumar ojha <rajeshkumarojha.dev@gmail.com>
2024-07-31 12:29:19 +0530

    Add files via upload

---
+++ PYTHON FULL STACK/PYTHON TUTORIAL/FIRST.py
@@ -0,0 +1,27 @@
+# print("hello world")
+
+# import calendar as cal
+# print(cal.calendar(2024))
+
+# import math
+# print(math.pi)
+# print(math.pi)
+# print(math.floor(pow(9,3)))
+# print(math.sqrt(9))
+
+# import random
+
+# random_number = random.randint(10, 20)
+# print(random_number)
+
+
+import pyautogui
+import time
+time.sleep(2)
+
+count=0
+while count<=500:
+ pyautogui.typewrite("Happy birth day bandaria")
+ pyautogui.press("enter")
+ count = count+1
+
